82a967267a4c845ba312e78c2efdb00f623e1fe6
Fraser Thompson <fthompson@modern.adi.co.nz>
2020-06-11 12:28:05 +1200

    Even more minimal

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 
 const App = () => (
   <div>
-    <CKEditor editor={ClassicEditor} data={"Text"} disabled={false} />
+    <CKEditor editor={ClassicEditor} />
   </div>
 );
 
